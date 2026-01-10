38b0105e36b7ec679e0203eefeaceb088da92b58
Local-Developer17 <umutakdenizli17@gmail.com>
2026-01-10 12:38:18 +0300

    [Inital Commit] - Installing Vue Project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1 v-if="state.isVisible">Your title is here!</h1>
+    <button @click="state.isVisible = !state.isVisible">Click Here</button>
+    <button @click="increment">Count is {{ state.count }}</button>
+  </div>
+</template>
+<script setup>
+import { ref, reactive } from 'vue'
+let state = reactive({
+  isVisible: false,
+  count: 0,
+})
+const increment = () => {
+  state.count++
+}
+</script>
+<style scoped></style>
